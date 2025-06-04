88e6c93a6c742f2de848a722aea1b05a362e367f
Renate <97016086@st.deltion.nl>
2025-06-04 14:41:54 +0200

    moest het bestand hernoemen, omdat je een class niet kan hernoemen in een controller

--- app/Http/Controllers/AttendanceRecordsController.php
+++ app/Http/Controllers/AttendanceRecordsController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers;
 use App\Models\AttendanceRecords;
 use Illuminate\Http\Request;
 
-class AttendanceRecords extends Controller
+class AttendanceRecordsController extends Controller
 {
 	public function index(Request $request) {
-		$query = AttendanceRecords::query();
 
-		
+
+
+
 
 	} 
 }
